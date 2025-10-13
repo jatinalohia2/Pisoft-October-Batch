5979965ca44d2d4c8680af43de9c3cb2f8865f9d
jatinalohia2 <jatinalohia2@gmail.com>
2025-10-13 14:27:36 +0530

    First commit

---
+++ src/main/java/com/pisoft/pisoft/Apple.java
@@ -0,0 +1,12 @@
+package com.pisoft.pisoft;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+
+    void eat(){
+        System.out.println("i am eating apple");
+    }
+
+}
---
+++ src/main/java/com/pisoft/pisoft/PisoftApplication.java
@@ -0,0 +1,21 @@
+package com.pisoft.pisoft;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisoftApplication implements CommandLineRunner {
+
+    @Autowired
+    Apple apple ;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisoftApplication.class, args);
+	}
+    @Override
+    public void run(String... args) throws Exception {
+        apple.eat();
+    }
+}
